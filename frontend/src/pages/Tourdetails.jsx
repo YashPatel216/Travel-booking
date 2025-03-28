d11fb8fab2cc341b3fe581009d176474f9c0ff10
TirthTilva <125015845+TirthTilva@users.noreply.github.com>
2025-03-28 22:36:06 +0530

    hello

---
+++ frontend/src/pages/Tourdetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tourdetails = () => {
+  return (
+    <div>tourdetails</div>
+  )
+}
+
+export default Tourdetails;
---
+++ frontend/src/router/Routers.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Routes,Route,Navigate} from 'react-router-dom'
+
+import Home from '../pages/Home';
+import Tours from '../pages/Tours';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Searchresult from '../pages/Searchresult';
+import Tourdetails from '../pages/Tourdetails';
+
+
+const routers = () => {
+  return (
+     <Routes>
+        <Route path='/' element={<Navigate to='/Home'/>} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/tours' element={<Tours/>} />
+        <Route path='/tours/:id' element={<Tourdetails/>} />
+        <Route path='/tours/search' element={<Searchresult/>} />
+
+     </Routes>
+  );
+};
+
+export default routers;
