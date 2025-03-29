6d38dd23126801dafed5644cf9116631240c0142
TirthTilva <125015845+TirthTilva@users.noreply.github.com>
2025-03-29 21:45:33 +0530

    home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import heroImg02 from '../assets/images/hero-img02.jpg';
 import heroVideo from '../assets/images/hero-video.mp4';
 import Subtitle from './../shared/Subtitle'
 import worldImg from '../assets/images/world.png'
+import SearchBar from '../shared/SearchBar';
 const Home = () => {
   return (
     <>
@@ -16,7 +17,7 @@ const Home = () => {
           <div className="hero__content">
             <div className="hero__subtitle d-flex align-items-center">
               <Subtitle subtitle={'Know Before You Go'}/>
-              <img src ={worldImg} alt="Image"/>
+              <img src ={worldImg} alt="Img"/>
             </div>
             <h1> Traveling Open door to creating <span className="highlights">Memories</span></h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex quidem iusto, nesciunt labore expedita inventore rem quos, aliquam consequuntur iste sit debitis deserunt! Illum architecto consectetur, nulla iste atque temporibus?</p>
@@ -24,7 +25,7 @@ const Home = () => {
           </Col>
          <Col lg='2'>
          <div className="hero__img-box">
-          <img src={heroImg01}/>
+          <img src={heroImg01} alt='img'/>
          </div>
          </Col>
          <Col lg='2'>
@@ -35,9 +36,10 @@ const Home = () => {
 
          <Col lg='2'>
          <div className="hero__img-box mt-5" >
-          <img src={heroImg02}/>
+          <img src={heroImg02} alt='img'/>
          </div>
          </Col>
+         <SearchBar/>
         </Row>
       </Container>
     </section>
