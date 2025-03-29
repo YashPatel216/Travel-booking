456b635a890ef4647d1c79f85b4facaa9f00db08
Yash Patel <yashpatel2165@gmail.com>
2025-03-29 17:47:46 +0530

    Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/home.css'
-import {container,row,col} from 'reactstrap';
-import heroImg from '../assests/images/hero-img01.jpg'
-import heroImg02 from '../assests/images/hero-img02.jpg'
-import heroVideo from '../assests/images/hero-video.jpg'
-
+import {Container,Row,Col} from 'reactstrap';
+import heroImg01 from '../assets/images/hero-img01.jpg';
+import heroImg02 from '../assets/images/hero-img02.jpg';
+import heroVideo from '../assets/images/hero-video.mp4';
+import Subtitle from './../shared/Subtitle'
+import worldImg from '../assets/images/world.png'
 const Home = () => {
   return (
     <>
@@ -12,13 +13,35 @@ const Home = () => {
       <Container>
         <Row>
           <Col lg='6'>
-          <div className="hero__content"></div>
+          <div className="hero__content">
+            <div className="hero__subtitle d-flex align-items-center">
+              <Subtitle subtitle={'Know Before You Go'}/>
+              <img src ={worldImg} alt="Image"/>
+            </div>
+            <h1> Traveling Open door to creating <span className="highlights">Memories</span></h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex quidem iusto, nesciunt labore expedita inventore rem quos, aliquam consequuntur iste sit debitis deserunt! Illum architecto consectetur, nulla iste atque temporibus?</p>
+          </div>
           </Col>
+         <Col lg='2'>
+         <div className="hero__img-box">
+          <img src={heroImg01}/>
+         </div>
+         </Col>
+         <Col lg='2'>
+         <div className="hero__img-box  mt-4">
+          <video src={heroVideo} controls/>
+         </div>
+         </Col>
+
+         <Col lg='2'>
+         <div className="hero__img-box mt-5" >
+          <img src={heroImg02}/>
+         </div>
+         </Col>
         </Row>
       </Container>
     </section>
     </>
   )
 }
-
 export default Home;
--- frontend/src/shared/Subtitle.jsx
+++ frontend/src/shared/Subtitle.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 const Subtitle =({subtitle}) =>{
     return (
-        <h3 className='section__subtitle'>{Subtitle}</h3>
+        <h3 className='section__subtitle'>{subtitle}</h3>
     )
 }
+export default Subtitle
