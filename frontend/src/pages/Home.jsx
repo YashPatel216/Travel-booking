bf2eb806f74e74cd53004665ebfee9143ae24362
Yash Patel <yashpatel2165@gmail.com>
2025-03-29 17:05:20 +0530

    shared

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import '../styles/home.css'
+import {container,row,col} from 'reactstrap';
+import heroImg from '../assests/images/hero-img01.jpg'
+import heroImg02 from '../assests/images/hero-img02.jpg'
+import heroVideo from '../assests/images/hero-video.jpg'
 
 const Home = () => {
   return (
-    <div>home</div>
+    <>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6'>
+          <div className="hero__content"></div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+    </>
   )
 }
 
