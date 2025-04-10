8a3fe5d6b0a0f7a29561a5c1f946a821a2bb0d34
Yash Patel <yashpatel2165@gmail.com>
2025-04-10 14:26:38 +0530

    tours

--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createTour, getAllTour, getSingleTour } from '../controllers/tourController.js';
+import { createTour, updateTour, getAllTour, deleteTour, getSingleTour } from '../controllers/tourController.js';
 
 
 const router=express.Router()
