469389ce15d1c179e3ec3d7467692880d5d99683
TirthTilva <125015845+TirthTilva@users.noreply.github.com>
2025-04-11 23:38:42 +0530

    done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,13 +7,13 @@ export const register =async(req,res)=>{
     try{
 
         //hashing password
-        const salt = bcrypt.genSaltSync(10);
-        const hash=bycrypt.hashSync(req.body.password,salt);
+        // const salt = bcrypt.genSaltSync(10);
+        // const hash=bycrypt.hashSync(req.body.password,salt);
 
         const newUser = new User({
             username:req.body.username,
             email:req.body.email,
-            password:hash,
+            password:req.body.password,
             photo:req.body.photo    
         }) 
 
@@ -27,10 +27,35 @@ export const register =async(req,res)=>{
 }
 //user login
 export const login =async(req,res)=>{
+    const email = req.body.email;
     try{
-
+    const user = await User.findOne({email})
+    // if user doesn't exist
+    if(!user){
+    return res.status(404).json({success : false, message: 'User not found'})
     }
-    catch(err){
+    // if user is exist then check the password or compare the password
+    const checkCorrectPassword=bycrypt.compare(req.body.password,user.password);
 
+    //if password is incorrect
+    if(!checkCorrectPassword){
+        return res.status(401).json({success:false,message:"Incorrect email or password"})
     }
+
+    const {password,role,...rest}=user._doc
+
+    //create jwt token
+    const token = jwt.sign({id:user._id,role:user.role},process.env.JWT_SECRET_KEY,{expiresIn:"15d"});
+
+
+    // set token in the browser cookies and send the response to the client
+    res.cookie('accessToken', token, {
+    httpOnly: true,
+    expires : token. expiresIn
+    }).status(200).json({success:true, message: 'successfully login', data:{ ...rest}} )
 }
+    catch(err){
+        res.status(404).json({success : false, message: 'Failed to login'})
+    }
+    
+};
