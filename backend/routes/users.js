941abd801cbe1a38e8b3569978f2777edabb432e
TirthTilva <125015845+TirthTilva@users.noreply.github.com>
2025-04-12 00:00:56 +0530

    latest

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,14 +1,17 @@
 import express from 'express'
 import { getSingleUser,deleteUser,updateUser, getAllUser } from '../controllers/userController.js';
+import { verifyUser } from '../utils/verifyToken.js';
 const router=express.Router()
 
+
+
 //update new user
-router.put('/:id',updateUser);
+router.put('/:id',verifyUser,updateUser);
 //delete new user
-router.delete('/:id',deleteUser);
+router.delete('/:id',verifyUser,deleteUser);
 //get single user
-router.get('/:id',getSingleUser);
+router.get('/:id',verifyUser,getSingleUser);
 //get all user
-router.get('/',getAllUser);
+router.get('/',verifyUser,getAllUser);
 
 export default router
