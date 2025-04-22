ca12f4477a7821f19876fb979e3df95ea99fcef4
TirthTilva <125015845+TirthTilva@users.noreply.github.com>
2025-04-22 10:30:24 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -39,8 +39,9 @@ app.get("/", (req, res) => {
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(cookieParser());
-app.use('/api/v1/users',userRoute)
+app.use('/api/users',userRoute)
 app.use('/api/v1/tours',tourRoute)
+app.use('/api/tours',tourRoute)
 app.use('/api/v1/auth',authRoute)
 app.use('/api/v1/review',reviewRoute)
 app.use('/api/v1/booking',bookingRoute)
