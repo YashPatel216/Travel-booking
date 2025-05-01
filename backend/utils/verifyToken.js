caf42abee8e5be72431c43a176e28a258ab13da4
TirthTilva <125015845+TirthTilva@users.noreply.github.com>
2025-05-01 11:02:00 +0530

    ww

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -1,19 +1,21 @@
 import jwt from 'jsonwebtoken'
-const verifyToken=(req,res,next)=>{
-    const token =req.cookies.accessToken
-    if(!token)
-    {
-        return res.status(401).json({success:false, message:'You are Not authorized'})
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+  
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ success: false, message: 'You are Not authorized' });
     }
-
-    jwt.verify(token,process.env.JWT_SECRET_KEY,(err,user)=>{
-        if(err){
-            return res.status(401).json({success:false, message:'token is invalid'})
-        }
-        req.user = user
-        next();
-    })
-}
+  
+    const token = authHeader.split(" ")[1];
+  
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+      if (err) {
+        return res.status(401).json({ success: false, message: 'Token is invalid' });
+      }
+      req.user = user;
+      next();
+    });
+  };
 
 export const verifyUser = (req,res,next)=>{
     verifyToken(req,res,next,()=>{
